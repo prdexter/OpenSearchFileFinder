df634f8b70fe61c8d27d13a2f3c5faddeaa42b19
Paul Dexter <paul.dexter@example.com>
2026-09-03 17:46:47 -0400

    Remove d:\endnote from DEFAULT_EXCLUDE_DIRS to allow indexing D:\Endnote

--- ingest_documents.py
+++ ingest_documents.py
@@ -82,7 +82,7 @@ DEFAULT_EXCLUDE_DIRS = {
     r'c:\windows', r'c:\program files', r'c:\program files (x86)',
     r'c:\programdata', r'$recycle.bin', r'system volume information',
     r'd:\active research\deidentifier\identified', r'deidentifier\identified',
-    r'd:\quicken', r'd:\endnote', r'd:\backups', r'd:\backups\documents',
+    r'd:\quicken', r'd:\backups', r'd:\backups\documents',
     r'd:\outlook files', r'd:\home videos and pictures',
     r'd:\pictures backup april 2010', r'd:\dtsearch index'
 }
